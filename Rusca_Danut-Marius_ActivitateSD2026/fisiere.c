3cb30c3040b5823f951e1a675b693d785c1bc686
Marius <marius@zeus.ro>
2026-03-13 18:06:52 +0200

    fisiere

--- Rusca_Danut-Marius_ActivitateSD2026/fisiere.c
+++ Rusca_Danut-Marius_ActivitateSD2026/fisiere.c
@@ -17,17 +17,29 @@ struct StructuraMasina {
 typedef struct StructuraMasina Masina;
 
 void afisareMasina(Masina masina) {
-	//afiseaza toate atributele unei masini
+	printf("ID: %d\n", masina.id);
+	printf("Numar usi: %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume Sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n", masina.serie);
 }
 
 void afisareVectorMasini(Masina* masini, int nrMasini) {
-	//afiseaza toate elemente de tip masina din vector
-	//prin apelarea functiei afisareMasina()
+	for (int i = 0; i < nrMasini; i++) {
+		afisareMasina(masini[i]);
+	}
 }
 
 void adaugaMasinaInVector(Masina** masini, int * nrMasini, Masina masinaNoua) {
-	//adauga in vectorul primit o noua masina pe care o primim ca parametru
-	//ATENTIE - se modifica numarul de masini din vector;
+	Masina* aux = (Masina*)malloc(sizeof(Masina)*(*nrMasini+1));
+	for (int i = 0; i < *nrMasini; i++) {
+		aux[i] = (*masini)[i];
+	}
+	aux[(*nrMasini)] = masinaNoua;
+	free(*masini);
+	(*masini) = aux;
+	(*nrMasini)++;
 }
 
 Masina citireMasinaFisier(FILE* file) {
