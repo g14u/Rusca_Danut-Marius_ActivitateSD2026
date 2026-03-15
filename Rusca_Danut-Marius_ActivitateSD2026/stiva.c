867d18d35a51ce30aa24c3cb19d592184db0d024
Marius <marius@zeus.ro>
2026-03-15 09:19:59 +0200

    update

---
+++ Rusca_Danut-Marius_ActivitateSD2026/stiva.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<malloc.h>
+
+// coada - queue - FIFO
+
+// stiva - stack - LIFO - inserarea si extragerea se fac de la acelasi capat
+// - inserarea se cheama push, extragerea (citire si stergere) pop
+// inserarea la inceput se face cu mai putine operatii
+
+
+
+
+int main() {
+
+
+	return 0;
+}
